283bf4acbb58b4f5bbce0f966992a1dea7b15479
Giuliano Collacchioni <elitexxl@hotmail.it>
2025-07-10 22:45:30 +0200

    ♻️ refactor(custom-event-handling): abstract component retrieval logic
    - create getComponent function to centralize component retrieval logic
    - update getRoute and getRouter to use getComponent for improved code reuse and readability

--- src/custom-event-handling/getRoute.ts
+++ src/custom-event-handling/getRoute.ts
@@ -1,13 +1,5 @@
-import { DOM_COMPONENT_INSTANCE_PROPERTY } from "./constants";
+import getComponent from "./getComponent";
 
 export default function getRoute(element: Element) {
-    if (!(element instanceof HTMLElement)) {
-        return null;
-    }
-
-    const riotComponent = element[DOM_COMPONENT_INSTANCE_PROPERTY];
-    return (riotComponent != null && riotComponent.name === "rhm-route" ?
-        riotComponent :
-        null
-    );
+    return getComponent(element, "rhm-route");
 }
--- src/custom-event-handling/getRouter.ts
+++ src/custom-event-handling/getRouter.ts
@@ -1,13 +1,5 @@
-import { DOM_COMPONENT_INSTANCE_PROPERTY } from "./constants";
+import getComponent from "./getComponent";
 
 export default function getRouter(element: Element) {
-    if (!(element instanceof HTMLElement)) {
-        return null;
-    }
-
-    const riotComponent = element[DOM_COMPONENT_INSTANCE_PROPERTY];
-    return (riotComponent != null && riotComponent.name === "rhm-router" ?
-        riotComponent : 
-        null
-    );
+    return getComponent(element, "rhm-router");
 }
