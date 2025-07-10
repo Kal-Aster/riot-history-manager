1828ef024a4039f872c963129c3dac568cb27325
Giuliano Collacchioni <elitexxl@hotmail.it>
2025-07-10 23:23:14 +0200

    ♻️ refactor(constants): remove unused constants
    - eliminate unused constants BASE, PARENT_ROUTE, PARENT_ROUTER, and ROUTE_PLACEHOLDER
    - clean up imports in rhm-route.riot to reflect constant removal

--- src/constants.ts
+++ src/constants.ts
@@ -1,17 +1,12 @@
 import { __ } from "riot";
 
 export const ROUTER: unique symbol = Symbol("router");
-export const BASE: unique symbol = Symbol("base");
-export const PARENT_ROUTE: unique symbol = Symbol("parent_route");
-export const PARENT_ROUTER: unique symbol = Symbol("parent-router");
 
 export const UNROUTE_METHOD: unique symbol = Symbol("unroute");
 
 export const CURRENT_ROUTE_INDEX: unique symbol = Symbol("current-route-index");
 export const LAST_ROUTED: unique symbol = Symbol("last-routed");
 export const TEST_LAST_ROUTED: unique symbol = Symbol("test-last-routed");
-
-export const ROUTE_PLACEHOLDER: unique symbol = Symbol("route-placeholder");
 
 export const IS_UNMOUNTING = Symbol("is-unmounting");
 
